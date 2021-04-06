50949f6a663085f42e3ed3562192635067c983e9
murilo <murilopaiva22@gmail.com>
2021-04-06 00:32:16 -0300

    Modularizando código

---
+++ ContaCorrente.js
@@ -0,0 +1,29 @@
+export class ContaCorrente{
+    agencia;
+    cliente;
+
+    _saldo = 0;
+    
+
+    sacar(valor){
+        if(this._saldo >= valor){
+            this._saldo -= valor;
+            return valor;
+        }
+    }
+
+    depositar(valor){
+        if(valor <= 0)
+        {
+            return;
+        }
+        this._saldo += valor;
+        
+    }
+
+    trasferir(valor, conta){
+        const valorSacado = this.sacar(valor);
+        conta.depositar(valorSacado);
+        
+    }
+}
--- index.js
+++ index.js
@@ -1,29 +1,5 @@
-class  Cliente{
-    nome;
-    cpf;
-}
-
-class ContaCorrente{
-    agencia;
-    _saldo = 0;
-
-    sacar(valor){
-        if(this._saldo >= valor){
-            this._saldo -= valor;
-            return valor;
-        }
-    }
-
-    depositar(valor){
-        if(valor <= 0)
-        {
-            return;
-        }
-        this._saldo += valor;
-        
-    }
-}
-
+import {Cliente} from "./Cliente.js"
+import {ContaCorrente} from "./ContaCorrente.js"
 
 const cliente1 = new Cliente();
 cliente1.nome = "Ricardo";
@@ -35,13 +11,15 @@ cliente2.cpf = 88822233309;
 
 const contaCorrenteRicardo = new ContaCorrente();
 contaCorrenteRicardo.agencia = 1001;
+contaCorrenteRicardo.cliente = cliente1;
+contaCorrenteRicardo.depositar(500);
 
+const conta2 = new ContaCorrente();
+conta2.cliente = cliente2;
+conta2.agencia = 102;
 
+let valor = 200;
+contaCorrenteRicardo.trasferir(200, conta2);
 
-contaCorrenteRicardo.depositar(100);
-
-const valorSacado = contaCorrenteRicardo.sacar(50);
-console.log(valorSacado);
-
-
-console.log(contaCorrenteRicardo);
+console.log("valor", valor)
+console.log(conta2);
