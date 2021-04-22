fc75ed61dc94f23cd36356070bfa79b8eb2cf97a
murilo <murilopaiva22@gmail.com>
2021-04-22 06:58:17 -0300

    Construtores e estáticos

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -1,8 +1,10 @@
 import {Cliente} from "./Cliente.js";
 
 export class ContaCorrente{
+    static numeroDeContas = 0;
     agencia;
     _cliente;
+    _saldo = 0;
 
     set cliente(novoValor){
         if(novoValor instanceof Cliente){
@@ -11,14 +13,22 @@ export class ContaCorrente{
         
     }
 
+
     get cliente(){
         return this._cliente;
     }
 
-    _saldo = 0;
+    
     
     get saldo(){
         return this._saldo;
+    }
+
+
+    constructor(agencia, cliente){
+        this.agencia = agencia;
+        this.cliente = cliente;
+        ContaCorrente.numeroDeContas += 1; 
     }
 
     sacar(valor){
